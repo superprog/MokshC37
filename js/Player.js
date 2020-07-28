0c8b1cab32750fad7580fe474d8b1267819d33cf
Moksh-Agarwal <mokshagarwal24august@gmail.com>
2020-07-28 21:35:13 +0530

    t

--- js/Player.js
+++ js/Player.js
@@ -1,9 +1,13 @@
 class Player {
-  constructor(){}
+  constructor(){
+    this.index=null;
+    this.name=null;
+    this.dist=0;
+  }
 
   getCount(){
     var playerCountRef = database.ref('playerCount');
-    playerCountRef.on("value",function(data){
+    playerCountRef.on("value",(data)=>{
       playerCount = data.val();
     })
   }
@@ -15,9 +19,36 @@ class Player {
   }
 
   update(name){
-    var playerIndex = "player" + playerCount;
+    var playerIndex = "Players/player" + this.index;
     database.ref(playerIndex).set({
-      name:name
+      name:this.name,
+      distance: this.dist
     });
+  }
+  static getPlayerInfo()
+  {
+    var playerRef= database.ref('Players');
+    playerRef.on("value",(data)=>
+    {
+      allPlayers=data.val();
+    })
+  }
+  async start()
+  {
+    if(gameState===0)
+    {
+      player= new Player();
+      var playerCountRef= await database.ref("playerCount").once("value");
+      if(playerCountRef.exists())
+      {
+        playerCount=playerCountRef.value();
+        player.getCount();
+        
+      }
+      form= new Form();
+      form.display();
+      
+      
+    }
   }
 }
